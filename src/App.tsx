8454919075d5fbf162d2c5f7a14329a7b81b82d5
kiwoon kim <kisoon0947@gmail.com>
2022-07-12 16:52:01 +0900

    update

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Todos from './components/Todos'
+import Todo from './models/todo'
+
+function App() {
+  const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')]
+
+  return (
+    <>
+      <Todos items={todos} />
+    </>
+  )
+}
+
+export default App
